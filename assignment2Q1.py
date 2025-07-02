a5ed69df28159597717b9b4d52a585131472c423
Garv Gupta <138745425+Debrogliehypo@users.noreply.github.com>
2025-07-02 22:17:09 +0530

    Add files via upload

---
+++ assignment2Q1.py
@@ -0,0 +1,10 @@
+
+
+x = int(input("enter the number : "))
+
+if(x/2 == int):
+
+    print(f"{x} is an even number")
+
+else:
+    print(f"{x} is a odd number ")
---
+++ assignment2Q2.py
@@ -0,0 +1,7 @@
+
+total = 0
+for i in range(0,51):
+    total += i
+
+print(total)
+
